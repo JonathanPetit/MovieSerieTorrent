500784813874abe65eeb17e5d66133864c36bbb4
Jonathan Petit <13026@student.ecam.be>
2016-03-30 11:35:11 +0200

    Add json option and progress parse function

--- test/Renamer.py
+++ test/Renamer.py
@@ -6,19 +6,35 @@ import re
 
 class Rename:
     def __init__(self):
+        self.types = None
         self.file = None
         self.result = None
         with open('terms_exclude.json') as data_file:
             self.bdd = json.load(data_file)
 
-    def parse(self, filename):
-        self.result = {}
-        self.file = self._delchars(filename)
-        return print(self.file)
-
     def _delchars(self, filename):
         clean_site = re.sub('{}'.format(self.bdd['sites']), ' ', filename)
         return re.sub(r'[^a-zA-Z0-9]+', ' ', clean_site).strip()
+
+    def parse(self, filename):
+        self.result = {}
+        self.file = self._delchars(filename)
+        for key, value in enumerate(self.bdd):
+            match = re.findall(self.bdd[value], filename)
+            try:
+                if value == 'episode' or value == 'season':
+                    self.result[value] = str(int(match[0]))
+                    self.types = True
+                else:
+                    self.result[value] = match[0]
+            except IndexError:
+                pass
+
+        if self.types == True:
+            self.result['type'] = 'serie'
+        else:
+            self.result['type'] = 'movie'
+        return print(self.file, '\n', self.result)
 
 
 if __name__ == '__main__':
