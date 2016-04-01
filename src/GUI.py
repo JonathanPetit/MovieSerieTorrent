caa909f37938a4eee92651741aec8a89c370ef1b
Jonathan Petit <13026@student.ecam.be>
2016-04-01 09:16:21 +0200

    Auto parser implementation

--- src/GUI.py
+++ src/GUI.py
@@ -9,32 +9,43 @@ class GUI(Frame):
         Frame.__init__(self, parent)
         self.parent = parent
         self._initUI()
+        self.directory = None
+
 
     def _initUI(self):
-        """ Initialise the UI """
         self.parent.title("Parse-Renamer V0.1.0")
         self.pack(fill=BOTH, expand=1)
         self.CreateWidgets()
 
     def CreateWidgets(self):
+        # Frame bouton
         load_dir_frame = Frame(self)
-        load_dir_frame.pack(side=RIGHT, anchor=N, fill=X)
-
-        button_load_dir = Button(load_dir_frame, text="Load Directory", command=self._LoadDir)
-        button_parse = Button(load_dir_frame, text="Parse", command=self._parse)
+        load_dir_frame.pack(side=RIGHT, anchor=W, fill=BOTH)
+        button_frame_name = LabelFrame(load_dir_frame, text="Function:")
+        button_frame_name.pack()
+        button_load_dir = Button(button_frame_name, text="Load Directory", command=self._LoadDir)
+        button_parse = Button(button_frame_name, text="Parse", command=self._parse)
         button_load_dir.pack(side=TOP)
         button_parse.pack(side=TOP)
 
+        # Frame Options
+        self.check_auto=IntVar()
+        options_frame = Frame(load_dir_frame)
+        options_frame.pack(side=TOP, anchor=W, fill=BOTH)
+        options_frame_name = LabelFrame(options_frame, text="Options:")
+        options_frame_name.pack()
+        self.checkbutton_auto = Checkbutton(options_frame_name, text="Auto Parse", variable=self.check_auto)
+        self.checkbutton_auto.pack(side=TOP)
 
+        # Frame listbox
         listebox = Frame(self)
         listebox.pack(fill=BOTH, expand=1)
-
         self.listebox_files = Listbox(listebox)
-        self.listebox_files.pack(side=TOP, fill=BOTH, expand=1, anchor=W)
+        self.listebox_files.pack(side=TOP, fill=BOTH, expand=1, padx=50, pady=50, anchor=W)
 
     def _LoadDir(self):
-        directory = filedialog.askdirectory()
-        for files in os.listdir(directory):
+        self.directory = filedialog.askdirectory()
+        for files in os.listdir(self.directory):
             if files.endswith('.DS_Store'):
                 pass
             else:
@@ -46,11 +57,17 @@ class GUI(Frame):
         return filename
 
     def _parse(self):
-        selection = self._GetListbox_selection()
-        return Parse().parse(selection)
+        if self.check_auto.get() == 1:
+            for files in range(self.listebox_files.size()):
+                result = self.listebox_files.get(files)
+                Parse().parse(result)
+        else:
+            selection = self._GetListbox_selection()
+            Parse().parse(selection)
 
 
 if __name__ == "__main__":
     window = Tk()
+    window.geometry("800x600+300+0")
     app = GUI(window)
     app.mainloop()
