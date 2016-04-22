64d0b1690694b090eb029826a5f8c4a7a883071a
Jonathan Petit <13026@student.ecam.be>
2016-04-22 16:29:34 +0200

    convert readme.mdin .rst on setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,15 @@ from setuptools import setup, find_packages
 import os
 
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
-with open('requirements.txt') as f:
-    install_requires = f.read().splitlines()
+with open('requirements.txt') as requirements:
+    install_requires = requirements.read().splitlines()
+
+try:
+    from pypandoc import convert
+    read_md = lambda f: convert(f, 'rst')
+except ImportError:
+    print("warning: pypandoc module not found, could not convert Markdown to RST")
+    read_md = lambda f: open(f, 'r').read()
 
 setup(
     name='MovieSerieTorrent',
@@ -15,7 +22,7 @@ setup(
     author="Petit Jonathan",
     author_email="petit.jonathan16@gmail.com",
     description="Parser and Renamer for torrents files (Movies and series)",
-    long_description=open('README.md').read(),
+    long_description=read_md('README.md'),
     include_package_data=True,
     url='https://github.com/JonathanPetit/Parser-Renamer',
     license= 'MIT',
