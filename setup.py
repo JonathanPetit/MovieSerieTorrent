87bc0cee8d74e5a9e3ab32f636d2c64146fc7d71
Jonathan Petit <13026@student.ecam.be>
2016-04-23 09:37:40 +0200

    use pypandoc for pypi readd

--- setup.py
+++ setup.py
@@ -1,21 +1,31 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
+import pypandoc
 import os
 
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 with open('requirements.txt') as requirements:
     install_requires = requirements.read().splitlines()
 
+try:
+    long_description = pypandoc.convert('README.md', 'rst')
+    long_description = long_description.replace("\r","")
+except OSError:
+    print("Pandoc not found. Long_description conversion failure.")
+    import io
+    with io.open('README.md', encoding="utf-8") as f:
+        long_description = f.read()
+
 setup(
     name='MovieSerieTorrent',
-    version='1.0.9',
+    version='1.0.11',
     packages=find_packages(),
     install_requires=install_requires,
     author="Petit Jonathan",
     author_email="petit.jonathan16@gmail.com",
     description="Parser and Renamer for torrents files (Movies and series)",
-    long_description=open('README.rst').read(),
+    long_description= long_description,
     include_package_data=True,
     url='https://github.com/JonathanPetit/Parser-Renamer',
     license= 'MIT',
